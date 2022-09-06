e1a5fedfa9369a97c0593d2113d98ab787295ce4
Administrator <Administrator@DELL-PC>
2022-09-06 12:13:03 +0530

    Notes App

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, {useEffect, useState} from 'react'
+import NotesLists from './components/NotesLists'
+import {nanoid} from 'nanoid'
+import Search from './components/Search'
+import Header from './components/Header'
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "8/21/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "8/22/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "8/23/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "8/24/2022"
+    },
+  ])
+
+  const [searchText, setSearchText] = useState('')
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'))
+
+    if(savedNotes){
+      setNotes(savedNotes)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('react-notes-app-data', JSON.stringify(notes))
+  }, [notes])
+
+  // adding note 
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    }
+    const newNotes = [...notes, newNote]
+    setNotes(newNotes)
+  }
+
+  // deleting note 
+  const deleteNote = (id) => {
+   const newNotes =  notes.filter((note) => note.id !== id)
+   setNotes(newNotes)
+  }
+
+  return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+    <div className ="container">
+      <Header handleDarkMode={setDarkMode}/>
+      <Search handleSearchNote={setSearchText}/>
+      <NotesLists 
+      notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} 
+      handleAddNote={addNote}
+      handleDeleteNote={deleteNote}
+      />
+    </div>
+    </div>
+  )
+}
+
+export default App
+
